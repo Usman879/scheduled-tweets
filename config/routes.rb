e9b28f2598a87b700882937c4be178ef295a4bcc
Usman879 <usmannoor519@gmail.com>
2023-04-11 11:24:48 +0500

    Following the rails tutorial untill the edit your password as lec 19

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,28 @@
 Rails.application.routes.draw do
   # GET /about
   get "about", to: "about#index"
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+  
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   root to: "main#index"
+
+  delete "logout", to: "sessions#destroy"
+
+
+
+
+
+
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
